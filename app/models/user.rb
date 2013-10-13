c4d8187d3c1d5788e14fd7b6dc1fceb0edf4799b
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-10-13 14:38:27 -0400

    added devise

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # attr_accessible :title, :body
+
+    attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :firstname, :lastname
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Hostelconnect::Application.routes.draw do
 
+  devise_for :users
+
   root :to => "static_pages#home_page"
 
   # The priority is based upon order of creation:
