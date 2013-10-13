92e3602dfe0e74f4da9e317e80d00d6ecfd67807
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-10-13 13:17:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hostelconnect::Application.config.secret_token = 'ea2715775f9e433d44bfc977c039e903753cafae1f494a0e224c949fd4eb1c15626b517587e9e5c9c8a8e32ecc042b0f35c9b8414774e63b070bc03140345d94'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hostelconnect::Application.config.session_store :cookie_store, key: '_hostelconnect_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hostelconnect::Application.config.session_store :active_record_store
